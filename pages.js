8dd5c6420690a482a96a682cb3c0db52c0980a4d
AL_SCHOOL <61769442+AL-SCHOOL@users.noreply.github.com>
2020-10-16 08:58:03 -0400

    Create pages.js

---
+++ pages.js
@@ -0,0 +1,8 @@
+function home() {document.location='index.html';}
+function science() {document.location='classes/science';}
+function freshman-advisory() {document.location='classes/freshman-advisory';}
+function english() {document.location='classes/english';}
+function band() {document.location='classes/band';}
+function history() {document.location='classes/history';} 
+function spanish() {document.location='classes/spanish';}
+function algebra() {document.location='classes/algebra';}
