70873298bc69a4c816312cce6443e089d57a155c
AL_SCHOOL <61769442+AL-SCHOOL@users.noreply.github.com>
2020-10-16 13:22:22 -0400

    Create classes.js

---
+++ js/classes.js
@@ -0,0 +1,8 @@
+function home() {location.href = "../index.html";}
+function science() {location.href = "science.html";}
+function freshman_advisory() {location.href = "freshman-advisory.html";}
+function english() {location.href = "english";}
+function band() {location.href = "band";}
+function history() {location.href = "history";}
+function spanish() {location.href = "spanish";}
+function algebra() {location.href = "algebra";}
