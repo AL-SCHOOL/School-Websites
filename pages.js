5f48c290a64d4f16b78cecad638606c80b3d875e
AL_SCHOOL <61769442+AL-SCHOOL@users.noreply.github.com>
2020-10-16 09:17:16 -0400

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 function home() {document.location='index.html';}
 function science() {document.location='classes/science';}
-function freshman-advisory() {document.location='classes/freshman-advisory';}
+function freshman_advisory() {document.location='classes/freshman-advisory';}
 function english() {document.location='classes/english';}
 function band() {document.location='classes/band';}
 function history() {document.location='classes/history';} 
