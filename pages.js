7de61ef5a5ebb658c1508ab7446dc863e4374397
AL_SCHOOL <61769442+AL-SCHOOL@users.noreply.github.com>
2020-10-16 10:32:47 -0400

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,8 +1,8 @@
-function home() {window.open("index.html", "_self");}
-function science() {window.open("classes/science.html", "_self");}
-function freshman_advisory() {window.open("classes/freshman-advisory", "_self");}
-function english() {window.open.("classes/english", "_self");}
-function band() {window.open("classes/band", "_self");}
-function history() {window.open("classes/history.html", "_self");} 
-function spanish() {window.open("classes/spanish.html", "_self");}
-function algebra() {window.open("classes/algebra.html", "_self");}
+function home() {window.open("index.html","_self");}
+function science() {window.open("classes/science.html","_self");}
+function freshman_advisory() {window.open("classes/freshman-advisory","_self");}
+function english() {window.open.("classes/english","_self");}
+function band() {window.open("classes/band","_self");}
+function history() {window.open("classes/history.html","_self");} 
+function spanish() {window.open("classes/spanish.html","_self");}
+function algebra() {window.open("classes/algebra.html","_self");}
