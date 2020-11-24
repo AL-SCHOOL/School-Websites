4e3118b9ff54fe305f7181d928c6954189a2f7ac
AL_SCHOOL <61769442+AL-SCHOOL@users.noreply.github.com>
2020-11-24 13:45:12 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,4 +6,5 @@ function band() {location.href = "classes/band";}
 function history() {location.href = "classes/history";}
 function spanish() {location.href = "classes/spanish";}
 function algebra() {location.href = "classes/algebra";}
+function archives() {location.href = "archives"}
 function about() {location.href = "about";}
